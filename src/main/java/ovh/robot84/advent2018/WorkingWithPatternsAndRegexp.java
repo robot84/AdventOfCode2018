dd6a4174ced8b024780744307cadee5a62f8ffa4
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-05 22:59:21 +0100

    One method more

--- src/main/java/ovh/robot84/advent2018/WorkingWithPatternsAndRegexp.java
+++ src/main/java/ovh/robot84/advent2018/WorkingWithPatternsAndRegexp.java
@@ -6,29 +6,38 @@ import java.util.regex.Pattern;
 
 public class WorkingWithPatternsAndRegexp {
 
+
     public static void main(String[] args) {
         WorkingWithPatternsAndRegexp main1 = new WorkingWithPatternsAndRegexp();
         main1.usePatternSplit();
-        System.out.println("");
 
+        System.out.println("");
         main1.useMatcherFind();
 
         System.out.println("");
+        main1.useFindRegex();
+
+        System.out.println("");
+        main1.useMatchesGroup();
+    }
+
+
+    private void useFindRegex() {
         System.out.println("FindRegex() method written by me:");
         String[] foundByFindRegexMethod;
-        foundByFindRegexMethod = main1.findRegex("(\\d+)", "#20 @ 214,215 216: 217x218");
+        foundByFindRegexMethod = FindRegex.findRegex("(\\d+)", "#20 @ 214,215: 217x218");
         for (String s : foundByFindRegexMethod) {
             System.out.println(s);
         }
     }
 
+
     void usePatternSplit() {
         //#1 @ 817,273: 26x26
-        String line = "#20 @ 214,215 216: 217x218";
+        String line = "#20 @ 214,215: 217x218";
         System.out.println("Input line:");
         System.out.println(line);
 
-        //Pattern p = Pattern.compile("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)");
         Pattern delimeter = Pattern.compile("([#@ ,:x])+");
         System.out.println("Using Pattern.split(); Matches:");
         String[] finding = delimeter.split(line);
@@ -37,9 +46,10 @@ public class WorkingWithPatternsAndRegexp {
         }
     }
 
+
     void useMatcherFind() {
         //#1 @ 817,273: 26x26
-        String line = "#20 @ 214,215 216: 217x218";
+        String line = "#20 @ 214,215: 217x218";
         System.out.println("Input line:");
         System.out.println(line);
 
@@ -52,16 +62,22 @@ public class WorkingWithPatternsAndRegexp {
         }
     }
 
-    String[] findRegex(String regex, String parsedLine) {
-        ArrayList<String> result = new ArrayList<>();
-        Pattern patternCompiled = Pattern.compile(regex);
-        Matcher matcher = patternCompiled.matcher(parsedLine);
 
-        while (matcher.find()) {
-            result.add(matcher.group());
+    void useMatchesGroup() {
+        //#1 @ 817,273: 26x26
+        String line = "#20 @ 214,215: 217x218";
+        System.out.println("Input line:");
+        System.out.println(line);
+
+        //Pattern p = Pattern.compile("#(\\d+) @ (\\d+),(\\d+):.*");
+        Pattern p = Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
+        Matcher m = p.matcher(line);
+        if (m.matches()) {
+            for (int i = 1; i <= m.groupCount(); i++) {
+                System.out.printf("m.group(%s): %s\n", i, m.group(i));
+            }
         }
-        String[] s = new String[result.size()];
-        s = result.toArray(s);
-        return s;
     }
+
+
 }
