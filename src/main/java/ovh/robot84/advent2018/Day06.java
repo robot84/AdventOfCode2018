b17d6a14b00094e6dfd3d9873270b3fe9ef833be
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-05 23:00:28 +0100

    New Template for Every New Day ;)

--- src/main/java/ovh/robot84/advent2018/Day06.java
+++ src/main/java/ovh/robot84/advent2018/Day06.java
@@ -1,15 +1,18 @@
 package ovh.robot84.advent2018;
 
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day06 {
     final static String INPUT_FILE2 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
-            "src\\main\\resources\\day04input2.txt";
-    final static int ARRAY_MAX_X = 10_000;
-    final static int ARRAY_MAX_Y = 10_000;
+            "src\\main\\resources\\day06input2.txt";
+    final static int ARRAY_MAX_X = 2_000;
+    final static int ARRAY_MAX_Y = 2_000;
     private final static String INPUT_FILE1 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
-            "src\\main\\resources\\day04input1.txt";
+            "src\\main\\resources\\day06input1.txt";
     HashMap<Character, Integer> hm1 = new HashMap();
     ArrayList<String> boxID = new ArrayList<>();
     String s;
@@ -24,14 +27,51 @@ public class Day06 {
         dayStar1.star1start();
     }
 
-    private void doForEachLineOfInput(String line) {
-
-    }
 
     private void star1start() {
-        String line;
+        String line = null;
+        int c = 0;
+
+        /*
+        Read input
+         */
         while ((line = myReader.get_line()) != null) {
-            doForEachLineOfInput(line);
+            System.out.println("Line: " + line);
+        }
+
+        while ((c = myReader.read()) != -1) {
+            System.out.print("!" + (char) c);
+        }
+
+        /*
+        Parse input
+         */
+        line = "#20 @ 214,215: 217x218";
+        Pattern p = Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
+        Matcher m = p.matcher(line);
+        if (m.matches()) {
+            for (int i = 1; i <= m.groupCount(); i++) {
+                System.out.printf("m.group(%s): %s\n", i, m.group(i));
+            }
+        }
+
+
+        Pattern pa = Pattern.compile("(\\d+)");
+        Matcher ma = pa.matcher(line);
+        System.out.println("Using Matcher.Find();");
+        while (ma.find()) {
+            System.out.print("m.find(): " + ma.group() + " ");
+            System.out.printf("Start index: %d, end index: %d\n", ma.start(), ma.end());
+        }
+
+
+        Pattern delimeter = Pattern.compile("([#@ ,:x])+");
+        System.out.println("Using Pattern.split(); Matches:");
+        String[] finding = delimeter.split(line);
+        for (int i = 0; i < finding.length; i++) {
+            System.out.printf("%d:%s\n", i, finding[i]);
         }
     }
+
+
 }
