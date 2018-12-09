3db6e1ce9567c19cda3b512d74facc965a8a744a
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-09 05:46:44 +0100

    Downloading input data from Internet added

--- src/main/java/ovh/robot84/advent2018/Day08.java
+++ src/main/java/ovh/robot84/advent2018/Day08.java
@@ -5,6 +5,15 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class implements an application that
+ * resolves AdventOfCode2018 task for particular day,
+ * as class name says
+ *
+ * @author Robert Ząbkiewicz
+ * @version 0.1.0
+ * @since 2018-12-01
+ */
 public class Day08 {
     final static String INPUT_FILE2 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
             "src\\main\\resources\\day08input2.txt";
@@ -23,57 +32,63 @@ public class Day08 {
 
     public static void main(String[] args) {
         Day08 dayStar1 = new Day08(INPUT_FILE1);
-        System.out.println("ARGS:" + args.length);
-        if (args.length > 0 && args[0].equals("-v")) Verbose.enableVerbose();
+        dayStar1.parsingProgramArguments(args);
+        //Verbose.mute();
         dayStar1.star1start();
     }
 
 
     private void star1start() {
         String line = null;
-        int c = 0;
 
-
-        /*
-        Read input
-         */
+        /*        Read input         */
         while ((line = myReader.get_line()) != null) {
             Verbose.print("Line: " + line);
-        }
-
-        while ((c = myReader.read()) != -1) {
-            Verbose.print("!" + (char) c);
-        }
+            /*while ((c = myReader.read()) != -1) {
+                Verbose.print("!" + (char) c);
+            */
 
         /*
         Parse input
          */
-        line = "#20 @ 214,215: 217x218";
-        Pattern p = Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
-        Matcher m = p.matcher(line);
-        if (m.matches()) {
-            for (int i = 1; i <= m.groupCount(); i++) {
-                Verbose.printf("m.group(%s): %s\n", i, m.group(i));
+
+           /* Pattern p = Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
+            Matcher m = p.matcher(line);
+            if (m.matches()) {
+                for (int i = 1; i <= m.groupCount(); i++) {
+                    Verbose.printf("m.group(%s): %s\n", i, m.group(i));
+                }
+            }*/
+
+
+           /*
+            Pattern pa = Pattern.compile("(\\d+)");
+            Matcher ma = pa.matcher(line);
+            System.out.println("Using Matcher.Find();");
+            while (ma.find()) {
+                Verbose.print("m.find(): " + ma.group() + " ");
+                Verbose.printf("Start index: %d, end index: %d\n", ma.start(), ma.end());
             }
-        }
+            */
 
-
-        Pattern pa = Pattern.compile("(\\d+)");
-        Matcher ma = pa.matcher(line);
-        System.out.println("Using Matcher.Find();");
-        while (ma.find()) {
-            Verbose.print("m.find(): " + ma.group() + " ");
-            Verbose.printf("Start index: %d, end index: %d\n", ma.start(), ma.end());
-        }
-
-
-        Pattern delimeter = Pattern.compile("([#@ ,:x])+");
-        Verbose.print("Using Pattern.split(); Matches:");
-        String[] finding = delimeter.split(line);
-        for (int i = 0; i < finding.length; i++) {
-            Verbose.printf("%d:%s\n", i, finding[i]);
+            /*
+            Pattern delimeter = Pattern.compile("([#@ ,:x])+");
+            Verbose.print("Using Pattern.split(); Matches:");
+            String[] finding = delimeter.split(line);
+            for (int i = 0; i < finding.length; i++) {
+                Verbose.printf("%d:%s\n", i, finding[i]);
+            }
+            */
         }
     }
 
+    private void parsingProgramArguments(String[] args) {
+        System.out.println("Program ARGS num:" + args.length + " ARGS:" + args);
+        if (args.length > 0 && args[0].equals("-v")) Verbose.enableVerbose();
+        if (args.length > 0 && args[0].equals("-vv")) Verbose.enableTwoLevelVerbose();
+        if (args.length > 0 && args[0].equals("-vvv")) Verbose.enableThreeLevelVerbose();
+        if (args.length > 0 && args[0].equals("--create-input-file"))
+            HelperMethods.getInputFileFromWWW(Integer.valueOf(this.getClass().getSimpleName().substring(3)), INPUT_FILE1);
+    }
 
 }
