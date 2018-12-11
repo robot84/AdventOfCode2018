85d51623980491ae121ff34e90c4b88a954cf7cf
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-11 23:01:03 +0100

    initial version

---
+++ src/main/java/ovh/robot84/advent2018/PerformanceTest.java
@@ -0,0 +1,21 @@
+package ovh.robot84.advent2018;
+
+public class PerformanceTest {
+private long startTime;
+
+
+
+public void startPerformanceTest() {
+    startTime = System.nanoTime();
+}
+
+
+
+public void stopPerformanceTest() {
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    if (duration > 1_000_000) System.out.printf("Time elapsed: %,6.3f ms\n", (duration / 1_000_000.0));
+    else System.out.printf("Time elapsed: %,6d ns\n", duration);
+}
+
+}
