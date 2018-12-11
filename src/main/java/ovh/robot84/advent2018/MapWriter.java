bf8d444cd4721554e69acdc83e36379ca4ada73c
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-11 22:59:54 +0100

    initial version

---
+++ src/main/java/ovh/robot84/advent2018/MapWriter.java
@@ -0,0 +1,95 @@
+package ovh.robot84.advent2018;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MapWriter {
+BufferedWriter fileWriter;
+String mapFilePath;
+
+
+
+void openMap(String mapFilePath) {
+    this.mapFilePath = mapFilePath;
+    fileWriter = null;
+    try {
+        fileWriter = new BufferedWriter(new FileWriter(this.mapFilePath));
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+void closeMap() {
+
+    try {
+        fileWriter.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+void writeMap(Boolean[][] mapa, int mapXsize, int mapYsize, String mapHeaderText) {
+    try {
+        fileWriter.write("\n" + mapHeaderText + "\n");
+    } catch (IOException e) {
+        System.out.println("Exception: Cannot write to map file " + this.mapFilePath);
+        e.printStackTrace();
+    }
+    for (int y = 0; y < mapXsize; y++) {
+        for (int x = 0; x < mapYsize; x++) {
+            try {
+                if (!mapa[y][x])
+                    fileWriter.write(('.'));
+                else {
+                    fileWriter.write(('X'));
+                }
+            } catch (IOException e) {
+                System.out.println("Exception: Cannot write value '" +
+                        mapa[y][x] + "' to map file " + this.mapFilePath);
+                e.printStackTrace();
+            }
+        }
+        try {
+            fileWriter.write("\n");
+        } catch (IOException e) {
+            System.out.println("Exception: Cannot write new line character to map file " + this.mapFilePath);
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+void writeMap(Integer[][] mapa, int mapXsize, int mapYsize, String mapHeaderText) {
+    try {
+        fileWriter.write("\n" + mapHeaderText + "\n");
+    } catch (IOException e) {
+        System.out.println("Exception: Cannot write to map file " + this.mapFilePath);
+        e.printStackTrace();
+    }
+    for (int y = 0; y < mapXsize; y++) {
+        for (int x = 0; x < mapYsize; x++) {
+            try {
+                fileWriter.write((mapa[y][x] + '.'));
+            } catch (IOException e) {
+                System.out.println("Exception: Cannot write value '" +
+                        mapa[y][x] + "' to map file " + this.mapFilePath);
+                e.printStackTrace();
+            }
+        }
+        try {
+            fileWriter.write("\n");
+        } catch (IOException e) {
+            System.out.println("Exception: Cannot write new line character to map file " + this.mapFilePath);
+            e.printStackTrace();
+        }
+    }
+}
+
+
+}
