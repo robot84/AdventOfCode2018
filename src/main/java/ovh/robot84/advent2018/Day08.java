e82f87d9823b27780b9cc61cf2d8da0e00aff2ac
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-15 19:49:13 +0100

    Fixed result printing bug

--- src/main/java/ovh/robot84/advent2018/Day08.java
+++ src/main/java/ovh/robot84/advent2018/Day08.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
  *
  * @author Robert Ząbkiewicz
  * @version 0.1.0
- * @since 2018-12-01
+ * @since 2018-12-15
  */
 public class Day08 {
 final static String INPUT_FILE2 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
@@ -47,40 +47,8 @@ private void star1start() {
     if ((line = myReader.get_line()) != null) {
         Verbose.print("Line: " + line);
     }
-
     Scanner scanner = new Scanner(line);
-    readNode(scanner);
-
     System.out.println("Result is: " + readNode(scanner));
-
-           /* Pattern p = Pattern.compile("^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$");
-            Matcher m = p.matcher(line);
-            if (m.matches()) {
-                for (int i = 1; i <= m.groupCount(); i++) {
-                    Verbose.printf("m.group(%s): %s\n", i, m.group(i));
-                }
-            }*/
-
-
-           /*
-            Pattern pa = Pattern.compile("(\\d+)");
-            Matcher ma = pa.matcher(line);
-            System.out.println("Using Matcher.Find();");
-            while (ma.find()) {
-                Verbose.print("m.find(): " + ma.group() + " ");
-                Verbose.printf("Start index: %d, end index: %d\n", ma.start(), ma.end());
-            }
-            */
-
-            /*
-            Pattern delimeter = Pattern.compile("([#@ ,:x])+");
-            Verbose.print("Using Pattern.split(); Matches:");
-            String[] finding = delimeter.split(line);
-            for (int i = 0; i < finding.length; i++) {
-                Verbose.printf("%d:%s\n", i, finding[i]);
-            }
-            */
-
 }
 
 
