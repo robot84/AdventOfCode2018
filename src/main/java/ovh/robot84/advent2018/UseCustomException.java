28a476d3b2a211fbab73ed5097cefefd72af1895
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-20 22:02:19 +0100

    Initial version

---
+++ src/main/java/ovh/robot84/advent2018/UseCustomException.java
@@ -0,0 +1,21 @@
+package ovh.robot84.advent2018;
+
+public class UseCustomException {
+
+MyException newExc = new MyException("This is a custom exception");
+
+
+public UseCustomException() throws MyException {
+    System.out.println("Hello Back Again with custom exception");
+    throw newExc;
+}
+
+
+public static void main(String args[]) {
+    try {
+        UseCustomException use = new UseCustomException();
+    } catch (MyException myEx) {
+        System.out.println("This is my custom exception:" + myEx.getMessage());
+    }
+}
+}
