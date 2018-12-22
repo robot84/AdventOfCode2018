e950eacc20088d3bf5e6b71b8d61b7b811bd1e29
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-23 00:28:29 +0100

    Overloaded constructor

--- src/main/java/ovh/robot84/advent2018/FuelCell3x3Field.java
+++ src/main/java/ovh/robot84/advent2018/FuelCell3x3Field.java
@@ -2,20 +2,39 @@ package ovh.robot84.advent2018;
 
 import javafx.util.Pair;
 
-public class FuelCell3x3Field implements Comparable<FuelCell3x3Field> {
+public class FuelCell3x3Field implements Comparable<FuelCell3x3Field>, Cloneable {
 
 int totalPwr;
 FuelCell cell;
 int x, y;
+int size = 3;
 
 
+FuelCell3x3Field() {
+    initiate(1, 1, 1, 1);
+}
 FuelCell3x3Field(int x, int y, int sn) {
+    initiate(x, y, sn);
+}
+
+
+FuelCell3x3Field(int x, int y, int sn, int size) {
+    initiate(x, y, sn, size);
+}
+
+
+public void initiate(int x, int y, int sn) {
+    initiate(x, y, sn, this.size);
+}
+
+
+public void initiate(int x, int y, int sn, int size) {
     this.x = x;
     this.y = y;
-    for (int y1 = 0; y1 < 3; y1++) {
-        for (int x1 = 0; x1 < 3; x1++) {
-            cell = new FuelCell(x + x1, y + y1, sn);
-            totalPwr += cell.getPwr();
+    for (int y1 = 0; y1 < size; y1++) {
+        for (int x1 = 0; x1 < size; x1++) {
+            //cell = new FuelCell(x + x1, y + y1, sn);
+            totalPwr += FuelCell.getPwr(x + x1, y + y1, sn);
         }
     }
 }
@@ -32,6 +51,18 @@ Pair<Integer, Integer> getXY() {
 
 
 @Override
+public Object clone() {
+    Object clon = null;
+    try {
+        clon = super.clone();
+    } catch (CloneNotSupportedException e) {
+        e.printStackTrace();
+    } finally {
+        return clon;
+    }
+}
+
+@Override
 public int compareTo(FuelCell3x3Field o) {
     return this.getTotalPwr() - o.getTotalPwr();
 }
@@ -45,13 +76,30 @@ int rackID;
 
 
 FuelCell(int x, int y, int sn) {
+    setPwr(x, y, sn);
+}
+
+
+static final int getPwr(final int x, final int y, final int sn) {
+    final int rackID;
+    int pwr;
     rackID = x + 10;
     pwr = (rackID * y + sn) * rackID;
     if (pwr / 100 > 0) pwr = (pwr / 100) % 10;
     else pwr = 0;
     pwr -= 5;
+    return pwr;
 }
 
+
+private int setPwr(int x, int y, int sn) {
+    rackID = x + 10;
+    pwr = (rackID * y + sn) * rackID;
+    if (pwr / 100 > 0) pwr = (pwr / 100) % 10;
+    else pwr = 0;
+    pwr -= 5;
+    return pwr;
+}
 
 int getPwr() {
     return this.pwr;
