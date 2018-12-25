2cf370c55088213f394be584bbc1c47d8632bc93
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-25 13:55:31 +0100

    Added import for Pattern and Matcher

--- src/main/java/ovh/robot84/advent2018/Template.java
+++ src/main/java/ovh/robot84/advent2018/Template.java
@@ -2,6 +2,8 @@ package ovh.robot84.advent2018;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class implements an application that
@@ -18,14 +20,17 @@ import java.util.HashMap;
  */
 
 public class Template {
-final static String INPUT_FILE2 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
-        "src\\main\\resources\\day08input2.txt";
-final static int ARRAY_MAX_X = 2_000;
-final static int ARRAY_MAX_Y = 2_000;
+
 private final static String INPUT_FILE1 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
         "src\\main\\resources\\day08input1.txt";
-HashMap<Character, Integer> hm1 = new HashMap();
-ArrayList<String> boxID = new ArrayList<>();
+private final static String INPUT_FILE2 = "C:\\Users\\qtcj47\\IdeaProjects\\AdventOfCode2018\\" +
+        "src\\main\\resources\\day08input2.txt";
+private final static int ARRAY_MAX_X = 2_000;
+private final static int ARRAY_MAX_Y = 2_000;
+private HashMap<Character, Integer> hm1 = new HashMap();
+private ArrayList<String> strings = new ArrayList<>();
+private ArrayList<Integer> integers = new ArrayList<Integer>();
+private ArrayList<Long> longs = new ArrayList<Long>();
 private MyReader myReader = new MyReader();
 
 
@@ -56,8 +61,7 @@ private void star1start() {
         Parse input
          */
 
-           /*
-           // look out when input has [ ] symbols. Quote them! Then in trouble start with:
+         /*  // look out when input has [ ] symbols. Quote them! Then in trouble start with:
            //Pattern p = Pattern.compile("\\s*(.*)\\s*");
            Pattern p = Pattern.compile("^#\\s*(\\d+)\\s*@\\s*(\\d+),(\\d+):\\s*(\\d+)x(\\d+)$");
             Matcher m = p.matcher(line);
@@ -66,11 +70,10 @@ private void star1start() {
                 for (int i = 1; i <= m.groupCount(); i++) {
                     Verbose.printf("m.group(%s): %s\n", i, m.group(i));
                 }
-            }
-            */
+            }*/
 
 
-           /*
+/*
             Pattern pa = Pattern.compile("(\\d+)");
             Matcher ma = pa.matcher(line);
             System.out.println("Using Matcher.Find();");
@@ -78,16 +81,14 @@ private void star1start() {
                 Verbose.print("m.find(): " + ma.group() + " ");
                 Verbose.printf("Start index: %d, end index: %d\n", ma.start(), ma.end());
             }
-            */
 
-            /*
             Pattern delimeter = Pattern.compile("([#@ ,:x])+");
             Verbose.print("Using Pattern.split(); Matches:");
             String[] finding = delimeter.split(line);
             for (int i = 0; i < finding.length; i++) {
                 Verbose.printf("%d:%s\n", i, finding[i]);
             }
-            */
+*/
     }
 }
 
