884aa90339cca21825f807d386ea541333d4c042
Robert Zabkiewicz <zabkiewicz.robert@gmail.com>
2018-12-28 23:45:30 +0100

    Added enableVerbose(verbose level) Deleted enableTwoLevelVerbose() and enableThreeLevelVerbose()

--- src/main/java/ovh/robot84/advent2018/Verbose.java
+++ src/main/java/ovh/robot84/advent2018/Verbose.java
@@ -1,7 +1,14 @@
 package ovh.robot84.advent2018;
 
-import java.sql.SQLOutput;
-
+/**
+ * This class implements 3-severity-level console message printer
+ * for use with -v -vv -vvv arguments with application command line
+ *
+ * @author Robert Ząbkiewicz
+ * @version 0.2.0
+ * @since 2018-12-01
+ * <p>
+ */
 public class Verbose {
 
 private static final int DEFAULT_ENABLED_ON_LEVEL_NUM = 1;
@@ -26,6 +33,8 @@ public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
 public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+private static final int MAX_VERBOSITY_LEVEL = 3;
+private static final int MIN_VERBOSITY_LEVEL = 1;
 
 private static boolean VERBOSE_ENABLE;
 private static boolean VERBOSE_MUTED;
@@ -35,7 +44,7 @@ private static boolean PROMPTS_ENABLED = true;
 private static String actualColor = ANSI_CYAN;
 private static Object[] promptForVerboseClassEvents = {"*** "};
 private static Object[] promptsForUserPrints = {">", ">>", ">>>"};
-private static int enabledOnLevel = DEFAULT_ENABLED_ON_LEVEL_NUM;
+private static int defaultVerbosityLevel = DEFAULT_ENABLED_ON_LEVEL_NUM;
 private static int verboseLevelForNextPrintF = DEFAULT_VERBOSE_LEVEL_FOR_UNMODIFIED_BY_setVerboseLevelForNextPrint_METHOD_USER_PRINTS;
 
 
@@ -51,7 +60,21 @@ public static void disablePrompts() {
 
 public static void enableVerbose() {
     VERBOSE_ENABLE = true;
-    printInColor("%sVerbose enabled.\n", promptForVerboseClassEvents);
+    printInColor("%sVerbose enabled on level ", promptForVerboseClassEvents);
+    printInColor(String.valueOf(defaultVerbosityLevel));
+    printInColor(".\n");
+}
+
+
+public static void enableVerbose(int enabledOnLevel) {
+    setVerboseDefaultLevel(enabledOnLevel);
+    Verbose.enableVerbose();
+}
+
+
+private static void setVerboseDefaultLevel(int enabledOnLevel) {
+    if (isVerboseLevelValid(enabledOnLevel))
+        Verbose.defaultVerbosityLevel = enabledOnLevel;
 }
 
 
@@ -90,7 +113,7 @@ public static void resetColor() {
 
 public static void print(String s) {
     if (VERBOSE_ENABLE && !VERBOSE_MUTED) {
-        if (Verbose.verboseLevelForNextPrintF <= Verbose.enabledOnLevel) {
+        if (Verbose.verboseLevelForNextPrintF <= Verbose.defaultVerbosityLevel) {
             Verbose.printInColor(s);
             Verbose.verboseLevelForNextPrintF =
                     DEFAULT_VERBOSE_LEVEL_FOR_UNMODIFIED_BY_setVerboseLevelForNextPrint_METHOD_USER_PRINTS;
@@ -101,14 +124,51 @@ public static void print(String s) {
 }
 
 
+/**
+ * Print with desired verbosity level
+ * @param level verbosity level. Message will be printed if actual verbosity level is
+ *              equal or greater that this variable value
+ * @param s
+ */
+public static void print(int level, String s) {
+    Verbose.setVerboseLevelForNextPrint(level);
+    print(s);
+}
+
+
+/**
+ * Print with actual verbosity level
+ *
+ * @param s
+ */
 public static void println(String s) {
     print(s + "\n");
 }
 
 
+/**
+ * Print in desired verbosity level with new line on end
+ *
+ * @param level verbosity level. Message will be printed if actual verbosity level is
+ *              *              equal or greater that this variable value
+ * @param s
+ */
+public static void println(int level, String s) {
+    Verbose.setVerboseLevelForNextPrint(level);
+    println(s);
+}
+
+
+/**
+ * Print to console with actual verbosity level.
+ * Prints exacly in form as when System.printf() invoked
+ *
+ * @param format as for System.printf()
+ * @param args   as for System.printf()
+ */
 public static void printf(String format, Object... args) {
     if (VERBOSE_ENABLE && !VERBOSE_MUTED) {
-        if (Verbose.verboseLevelForNextPrintF <= Verbose.enabledOnLevel) {
+        if (Verbose.verboseLevelForNextPrintF <= Verbose.defaultVerbosityLevel) {
             // TODO change String to StringBuilder
             if (PROMPTS_ENABLED) {
                 String formatWithPrompt = "%s ".concat(format);
@@ -130,6 +190,20 @@ public static void printf(String format, Object... args) {
 }
 
 
+/**
+ * Print to console with desired verbosity level.
+ *
+ * @param level  verbosity level. Message will be printed if actual verbosity level is
+ *               *              equal or greater that this variable value
+ * @param format - as for System.printf()
+ * @param args   - as for System.printf()
+ */
+public static void printf(int level, String format, Object... args) {
+    Verbose.setVerboseLevelForNextPrint(level);
+    printf(format, args);
+
+}
+
 private static void printInColor(String format, Object[] args) {
     setColor();
     System.out.format(format, args);
@@ -144,35 +218,20 @@ private static void printInColor(String text) {
 }
 
 
-public static void enableMultiLevelVerbose(int level) {
-    enableVerbose();
-    Verbose.enabledOnLevel = level;
-    printInColor("%sMulti level verbose enabled.\n", promptForVerboseClassEvents);
-}
-
-
-public static void disableMultiLevelVerbose() {
-    MULTI_LEVEL_VERBOSE_ENABLED = false;
-}
-
-
 public static void setVerboseLevelForNextPrint(int verboseLevel) {
-    Verbose.verboseLevelForNextPrintF = verboseLevel;
+    if(isVerboseLevelValid(verboseLevel))
+        Verbose.verboseLevelForNextPrintF = verboseLevel;
+}
+
+
+private static boolean isVerboseLevelValid(int verboseLevel) {
+    if (verboseLevel >= MIN_VERBOSITY_LEVEL && verboseLevel <= MAX_VERBOSITY_LEVEL) return true;
+    else return false;
 }
 
 
 public static int getVerboseLevelForNextPrintF() {
     return Verbose.verboseLevelForNextPrintF;
-}
-
-
-public static void enableTwoLevelVerbose() {
-    enableMultiLevelVerbose(2);
-}
-
-
-public static void enableThreeLevelVerbose() {
-    enableMultiLevelVerbose(3);
 }
 
 
